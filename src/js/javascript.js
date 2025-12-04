8d2f72176c65bc5e380ba24e7f136d1189766d88
ananthusankar1729 <211710819+ananthusankar1729@users.noreply.github.com>
2025-12-04 14:04:11 +0000

    Grid made using input size

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,16 +1,22 @@
-let x = 16;
+let x=0;
 let height = 98/10
 let width = 98/10
-const container = document.querySelector(".container")
-for (let i = 0; i < x; i++) {
-    let box = document.createElement("div");
-    box.classList.add("box");
-    container.appendChild(box);
-    for (let a = 0; a < x; a++) {
-        let grid = document.createElement("div");
-        grid.classList.add("grid")
-        box.appendChild(grid)
-        
-    }
-        
+const container = document.querySelector(".container");
+function makeGrid(x){
+    for (let i = 0; i < x; i++) {
+        let box = document.createElement("div");
+        box.classList.add("box");
+        container.appendChild(box);
+        for (let a = 0; a < x; a++) {
+            let grid = document.createElement("div");
+            grid.classList.add("grid");
+            box.appendChild(grid);
+        }
+    }   
 }
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => { x = parseInt(prompt("enter the grid size:", x));
+    container.innerHTML = "";
+    makeGrid(x);
+});
+
