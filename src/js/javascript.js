8b073ac9e5dde634f6986cff2ad1ef6ded2c3216
ananthusankar1729 <211710819+ananthusankar1729@users.noreply.github.com>
2025-12-04 14:21:24 +0000

    Add limit 1 to 100 for size

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -3,19 +3,24 @@ let height = 98/10
 let width = 98/10
 const container = document.querySelector(".container");
 function makeGrid(x){
-    for (let i = 0; i < x; i++) {
-        let box = document.createElement("div");
-        box.classList.add("box");
-        container.appendChild(box);
-        for (let a = 0; a < x; a++) {
-            let grid = document.createElement("div");
-            grid.classList.add("grid");
-            box.appendChild(grid);
-        }
-    }   
+    if (x<=100) {
+        for (let i = 0; i < x; i++) {
+            let box = document.createElement("div");
+            box.classList.add("box");
+            container.appendChild(box);
+            for (let a = 0; a < x; a++) {
+                let grid = document.createElement("div");
+                grid.classList.add("grid");
+                box.appendChild(grid);
+            }
+        }   
+        
+    } else {
+        alert("Enter a size between (1 - 100)");
+    }
 }
 const btn = document.querySelector("button");
-btn.addEventListener("click", () => { x = parseInt(prompt("enter the grid size:", x));
+btn.addEventListener("click", () => { x = parseInt(prompt("enter the grid size (1 - 100):", x));
     container.innerHTML = "";
     makeGrid(x);
 });
